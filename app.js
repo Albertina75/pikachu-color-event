33d2283f5219c70021e7d9046991a6c7ab0b9b22
Albertina75 <145276835+Albertina75@users.noreply.github.com>
2023-10-25 01:48:44 +0200

    pikachu cambia el color de su fondo

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // Pika-pika suerte!
+
+
+
+//selecciono los cuadros de colores
+
+let red = document.querySelector('#red');
+let blue = document.querySelector('#blue');
+let green = document.querySelector('#green');
+let sangria = document.querySelector('#sangria');
+
+//selecciono imagen Pikachu por su id
+
+let pikachu = document.querySelector('#pikachu');
+
+//selecciono contenedor principal que contiene los cuadros de colores, asocio el evento click
+
+let buttons = document.querySelector('#colors');
+
+//creo el evento para que sea ejecutado la función que toma event como argumento
+
+buttons.addEventListener('click', function (event) {
+    console.log("El color pulsado es el color: " + event.target.id); console.log("El color pulsado es el color: " + event.target.id);
+
+    //obtenemos el color del elemento que se hizo clic para asignar sus estilos incluido el color de fondo de Pikachu para así obtener el color seleccionado 
+    
+    const color = window.getComputedStyle(event.target).backgroundColor;
+    pikachu.style.backgroundColor = color;
+
+
+});
+
